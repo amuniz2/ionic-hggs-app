7845e04298518c3e262bd5c5bc680130ec9eb5c9
unknown <amuniz1027@gmail.com>
2020-03-02 21:34:48 -0500

    temp

--- src/app/modules/shopping/store/shopping.reducers.ts
+++ src/app/modules/shopping/store/shopping.reducers.ts
@@ -3,10 +3,37 @@ import {EntityState} from '@ngrx/entity';
 import {ShoppingItem} from '../../../model/shopping-item';
 import * as fromAdapter from './shopping.adapter';
 
+export class SectionItems {
+  section: string;
+  items: ShoppingItem[];
+  constructor(aisle: string, items: ShoppingItem[]) {
+    this.section = (items && items.length > 0) ? items[0].location.section : '';
+    this.items = items.filter((item) => item.location.section === this.section && item.location.aisle === aisle);
+  }
+}
 
+export class AisleItems {
+  aisle: string;
+  items: ShoppingItem[];
+  sections: SectionItems[];
+
+  constructor(items: ShoppingItem[]) {
+    this.aisle = (items && items.length > 0) ? items[0].location.aisle : '';
+    const itemsInAisle =  items.filter((item) => item.location.aisle === this.aisle);
+    this.items = itemsInAisle.filter((item) => item.location.aisle === this.aisle && !item.location.section);
+    const itemsInSectionsWithinAisle = itemsInAisle.filter((item) => item.location.section);
+    itemsInSectionsWithinAisle.forEach(item => {
+
+      this.sections.push(new SectionItems(item.location.section, itemsInSectionsWithinAisle));
+    })
+
+  }
+}
 export interface ShoppingListState  {
   groceryStoreId: number;
   shoppingItems: ShoppingItem[];
+  aisles: AisleItems[];
+  sections: SectionItems[];
   loading: boolean;
   error: Error;
 }
@@ -19,8 +46,21 @@ export const initialShoppingListManagementState: ShoppingListManagementState = {
   shoppingLists: fromAdapter.shoppingListAdapter.getInitialState(),
 };
 
+class ShoppingListByLocation {
+  groceryStoreId: number;
+  shoppingItems: ShoppingItem[];
+  aisles: AisleItems[];
+  sections: SectionItems[];
+  loading: boolean;
+  error: Error;
+
+  constructor(shoppingList: ShoppingItem[]) {
+  }
+}
+
 export function shoppingListManagementReducer(state = initialShoppingListManagementState,
                                               action: ShoppingActions): ShoppingListManagementState  {
+  convertToShoppingListDictionary(action.shoppingList);
   switch (action.type) {
     case ShoppingActionTypes.LoadShoppingListSucceeded: {
       return {
